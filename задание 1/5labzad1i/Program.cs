468ff1665ef57e7c13ed40d178f5e1b90c8cee0f
AFAPI <miklasmatvej@gmail.com>
2023-01-10 17:29:23 +0300

    1com

---
+++ задание 1/5labzad1i/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите n = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < a.Length; ++i)
+            {
+                Console.Write($"a[{i}]= ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Разность максимального и минимального элементов: {a.Max()-a.Min()}");
+        }
+    }
+}
